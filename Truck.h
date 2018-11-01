9de6afa5d1c3aeeaa0633eb5cc4bf8568e1664fd
MahamZehra <31242906+MahamZehra@users.noreply.github.com>
2018-11-01 17:21:40 +0500

    Create Truck.h

---
+++ Truck.h
@@ -0,0 +1,13 @@
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include "Vehicle.h"
+
+class Truck : public Vehicle
+{
+private:
+public:
+    Truck();
+    Truck(SDL_Texture* image, int x, int y);
+    ~Truck();
+    void Draw();
+};
