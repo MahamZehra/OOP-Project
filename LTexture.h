cc12f3c0ee4d37c96b50c140f0c22a16b80c0812
Kainat Abbasi <33037474+04051@users.noreply.github.com>
2018-11-21 03:12:47 -0800

    Update LTexture.h

--- LTexture.h
+++ LTexture.h
@@ -1,10 +1,12 @@
 #pragma once
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <iostream>
 
-//Texture wrapper class
+#include<SDL.h>
+#include<SDL_image.h>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
 class LTexture
 {
 private:
@@ -12,22 +14,16 @@ private:
     int width;
     int height;
 
-protected:
-
 public:
     LTexture();
     ~LTexture();
-    bool LoadFromFile( std::string path, SDL_Renderer* gRenderer );
-    void Free();    //Deallocates texture
-    //Set color modulation
-    void SetColor( Uint8 red, Uint8 green, Uint8 blue );
+    bool LoadFromFile(string, SDL_Renderer*, bool flagColorKey = true, Uint8 redColorKey = 0, Uint8 greenColorKey = 0xFF, Uint8 blueColorKey = 0xFF);        ///takes arguments to ColorKey
+    void RenderTexture(int x, int y, SDL_Renderer* gRenderer, SDL_Rect* clip = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE, double angle = 0.0, SDL_Point* center = NULL, float scale = 1);
+    void Free();
     //Set blending
-    void SetBlendMode( SDL_BlendMode blending );
+    void setBlendMode( SDL_BlendMode blending );
     //Set alpha modulation
-    void SetAlpha( Uint8 alpha );
-    void Render( int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL,
-                SDL_RendererFlip flip = SDL_FLIP_NONE, SDL_Renderer* gRenderer = NULL );
+    void setAlpha( Uint8 alpha );
     int GetWidth();
     int GetHeight();
-
 };
