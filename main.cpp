89ed46aeb1af9731f1e568d328b0c14389b1efe8
Kainat Abbasi <33037474+04051@users.noreply.github.com>
2018-11-24 07:22:01 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,104 +1,36 @@
-#include <iostream>
-#include "LTexture.h"
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
+#include <stdio.h>
+#include <string>
+#include "string.h"
+#include "LTexture.h"
+#include <iostream>
+#include "Character.h"
+
 
 using namespace std;
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 650;
+LTexture ButtonTexture;
+LTexture CharacterTexture;
+
+//Starts up SDL and creates window
 bool init();
-bool loadMedia();
+
+//Loads media
+
+
 void close();
-void setAlpha();
 
+//Loads individual image as texture
+
+//The window we'll be rendering to
 SDL_Window* gWindow = NULL;
+
+//The window renderer
 SDL_Renderer* gRenderer = NULL;
-LTexture gFontTexture;
-const int SCREEN_WIDTH =  500;
-const int SCREEN_HEIGHT = 200;
-Uint8 a = 255; //one component is set to full opaque
-Uint8 b = 0; //other is full transparent
-Uint8 button = 170; //for controlling alpha value of other textures
-Uint8 button_screen = 200; //for controlling alpha value of other textures
-int main(int argv, char** args )
-{
-    //Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
-            //flags to check which screen is running
-            long int frame = 0;
 
-            bool mouseClicked = false; //flag indicating mouse click
-            //Button* buttons = NULL;   //will hold button(s) on the screen
-            int x,y;
-			SDL_Event e;
-			//While application is running
-			while( !quit )
-			{
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-					SDL_GetMouseState(&x,&y);
-
-                    if (e.type==SDL_MOUSEBUTTONDOWN)
-                    {
-                        if(e.button.button==SDL_BUTTON_LEFT)
-                            mouseClicked=true;
-                            cout<<"hi"<<endl;
-                    }
-                    /*
-                    if (e.type == SDL_MOUSEMOTION) //to handle all mouse motion events for all screens
-                    {
-                        GameScreen.mouseMotionEvents(x,y);
-                        mainMenu.mouseMotionEvents(x,y);
-                        PauseScreen.mouseMotionEvents(x,y);
-                        QuitScreen.mouseMotionEvents(x,y);
-                        gameOver.mouseMotionEvents(x,y);
-                    }
-                    if(e.type == SDL_MOUSEBUTTONUP && mouseClicked) //tp handle all mouse click events for all screens
-                    {
-
-                        GameScreen.mouseClickEvents(x,y,buttonClick);
-                        mainMenu.mouseClickEvents(x,y,buttonClick);
-                        PauseScreen.mouseClickEvents(x,y,buttonClick);
-                        QuitScreen.mouseClickEvents(x,y,buttonClick);
-                        gameOver.mouseClickEvents(x,y,buttonClick);
-                        mouseClicked = false;
-                    }*/
-
-                //Clear screen
-				SDL_SetRenderDrawColor( gRenderer,  0, 0, 0, 0 );
-				SDL_RenderClear( gRenderer );
-
-
-
-				//Update screen
-				SDL_RenderPresent( gRenderer );
-			}
-		}
-	}
-	//Free resources and close SDL
-	close();
-    return 0;
-}
-}
 bool init()
 {
 	///Initialization flag
@@ -138,8 +70,7 @@ bool init()
 			else
 			{
 				///Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 0 );
-
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				//Initialize PNG loading
 				int imgFlags = IMG_INIT_PNG;
 				if( !( IMG_Init( imgFlags ) & imgFlags ) )
@@ -151,10 +82,6 @@ bool init()
 			}
 		}
 	}
-	/*if( Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1 )
-    {
-        success = false;
-    }*/
 	return success;
 }
 
@@ -162,22 +89,32 @@ bool loadMedia()
 {
 	///Loading success flag
 	bool success = true;
-	if( !gFontTexture.LoadFromFile( "button.png", gRenderer) )
+	if( !ButtonTexture.loadTexture( "button_see.png", gRenderer) )
 	{
-		printf( "Failed to load sprite sheet texture!\n" );
+		printf( "Failed to load buttons!\n" );
 		success = false;
 	}
+
+	if ( !CharacterTexture.loadTexture("Alphabets.jpg", gRenderer))
+    {
+        printf( "Failed to load alphabets!\n" );
+		success = false;
+
+    }
 	else
     {
         ///Set standard alpha blending
-        gFontTexture.setBlendMode( SDL_BLENDMODE_BLEND );
+        ButtonTexture.setBlendMode( SDL_BLENDMODE_BLEND );
     }
     return success;
 }
+
+
+
 void close()
 {
-	///Free loaded images
-    gFontTexture.Free();
+	///free loaded images
+    ButtonTexture.free();
 
 	///Destroy window
 	SDL_DestroyRenderer( gRenderer );
@@ -189,27 +126,68 @@ void close()
 	IMG_Quit();
 	SDL_Quit();
 }
-void setAlpha()
+int main(int argv, char** args )
 {
-    ///Cap if below 0
-    if( a - 3 < 0 )
-    {
-        a = 0;
-    }
-    ///Decrement otherwise
-    else
-    {
-        a -= 3;
-    }
-    ///Cap if above 255
-    if( b+ 3 > 255 )
-    {
-        b=255;
-    }
-    ///Increment otherwise
-    else
-    {
-        b += 3;
-    }
+    //Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+		    ButtonTexture.RenderTexture(50, 100, gRenderer);
+		    Character obj(&CharacterTexture, 80, 150, 97);
+		    obj.Render(gRenderer);
+
+			//Main loop flag
+			bool quit = false;
+
+            //flags to check which screen is running
+            long int frame = 0;
+
+            bool mouseClicked = false; //flag indicating mouse click
+            //Button* buttons = NULL;   //will hold button(s) on the screen
+            int x,y;
+			SDL_Event e;
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+
+
+					SDL_GetMouseState(&x,&y);
+
+                    if (e.type==SDL_MOUSEBUTTONDOWN)
+                    {
+                        if(e.button.button==SDL_BUTTON_LEFT)
+                            mouseClicked=true;
+                            cout<<"Mouse Clicked"<<endl;
+                    }
+
+                    SDL_SetRenderDrawColor(gRenderer, 50, 230, 230, 2);
+                    //SDL_RenderClear(gRenderer);
+                    SDL_RenderPresent(gRenderer);
+			}
+		}
+	}
+	//free resources and close SDL
+	close();
+    return 0;
 }
+}
+
 
