c556733f85d9ecfef98f690012dc481aa3d630ad
Kainat Abbasi <33037474+04051@users.noreply.github.com>
2018-11-24 06:16:54 -0800

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdio.h>
@@ -6,25 +9,23 @@
 #include"LTexture.h"
 #include"Point.h"
 
-//---------------------------------CHARACTER CLASS-----------------------------//
 class Character
 {
-    public:
-        Character();
-        Character(LTexture* image, float x, float y, int ascii_value);
-        ~Character();
-        void Render(SDL_Renderer* gRenderer, bool debug);
-        void operator = (const Character& cpy);
-
-    protected:
-
-    private:
-        Point position;
-        int ascii_value;
-        int width;
-        int height;
-        SDL_Rect sprite_clip;
-        LTexture* sprite;
+private:
+    Point m_position; //Position of the Character
+    int m_character_value = 0; //ascii value of the Character
+    int m_width; //width of character
+    int m_height; //height of character
+    SDL_Rect m_spriteClips; //clip specific to the Character
+    LTexture* m_spriteSheetTexture; //font image
+public:
+    Character();
+    Character(LTexture* image, float x, float y, int ascii);
+    ~Character();
+    void Render(SDL_Renderer* gRenderer);
+    void operator = (const Character& cpy); //operator overloading for assignment operatot
 };
+
+
 
 #endif // CHARACTER_H
